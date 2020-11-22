808d9bcb6480e6d2e48e9c76b05030ee8f22b3ae
Karan Shah <karanshah343@gmail.com>
2020-11-21 22:06:44 -0500

    added theme toggling switch and its styling

---
+++ src/compenents/themeToggler/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from "styled-components"
+import Toggle from 'react-toggle'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
+
+import "react-toggle/style.css"
+import './styles.scss'
+
+const ThemeToggler = ({ theme, toggleTheme }) => {
+  return (
+    <Toggle checked={theme === 'light'} icons={{
+      checked: <p><FontAwesomeIcon icon={faSun} size='lg' /></p>,
+      unchecked: <p><FontAwesomeIcon icon={faMoon} size='lg' /></p>
+    }}
+      onChange={toggleTheme} />
+  );
+};
+
+export default ThemeToggler;
