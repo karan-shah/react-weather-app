92bd32596aa7214a4d49d07a6fd11179209b8aca
Karan Shah <karanshah343@gmail.com>
2020-12-11 20:14:44 -0500

    removed eslint n prettier configs

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -330,16 +330,16 @@ module.exports = function (webpackEnv) {
           test: /\.(js|mjs|jsx|ts|tsx)$/,
           enforce: 'pre',
           use: [
-            {
-              options: {
-                cache: true,
-                formatter: require.resolve('react-dev-utils/eslintFormatter'),
-                eslintPath: require.resolve('eslint'),
-                resolvePluginsRelativeTo: __dirname,
+            // {
+            //   options: {
+            //     cache: true,
+            //     formatter: require.resolve('react-dev-utils/eslintFormatter'),
+            //     eslintPath: require.resolve('eslint'),
+            //     resolvePluginsRelativeTo: __dirname,
 
-              },
-              loader: require.resolve('eslint-loader'),
-            },
+            //   },
+            //   loader: require.resolve('eslint-loader'),
+            // },
           ],
           include: paths.appSrc,
         },
